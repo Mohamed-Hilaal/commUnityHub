269668690a479223d3e2b6b9de52f371ea8fd5bf
Mohamed Hilaal <87385838+Mohamed-Hilaal@users.noreply.github.com>
2024-07-22 18:54:29 +0530

    dashboard route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './Auth/Login'
 import Registration from './Profile/Registration'
+import Dashboard from './Dashboard'
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="" element={<Login />}/>
           <Route path="/profile_registration" element={<Registration />}/>
+          <Route path="/dashboard" element={<Dashboard />}/>
         </Routes>
       </BrowserRouter>
     </div>
