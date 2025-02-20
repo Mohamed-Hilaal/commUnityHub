b6897d5fe5fa7711588275104c06afa049707190
Mohamed Hilaal <87385838+Mohamed-Hilaal@users.noreply.github.com>
2025-02-20 07:42:43 +0530

    fix for current unity

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     belongs_to :account
     has_and_belongs_to_many :unities
+    belongs_to :current_unity, class_name: "Unity", optional: true
 
     validates :username, presence: true, uniqueness: true
     validates :google_id, presence: true, uniqueness: true
