f9fea5e1f5334ba727c09c55c754a12afbd2457d
Mohamed Hilaal <87385838+Mohamed-Hilaal@users.noreply.github.com>
2025-03-09 15:06:25 +0530

    backend wireup for chat

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
     belongs_to :account
-    has_and_belongs_to_many :unities
-    belongs_to :current_unity, class_name: "Unity", optional: true
     has_many :unity_posts 
+    has_and_belongs_to_many :unities
+    has_many :unity_chat_memberships, dependent: :destroy
+    has_many :unity_chats, through: :unity_chat_memberships, source: :unity_chat
+    belongs_to :current_unity, class_name: "Unity", optional: true
 
     validates :username, presence: true, uniqueness: true
     validates :google_id, presence: true, uniqueness: true
