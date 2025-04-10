a907872bc1d83bad3de3be5827ad713c8d565000
Mohamed Hilaal <87385838+Mohamed-Hilaal@users.noreply.github.com>
2025-04-10 07:57:13 +0530

    unity name in header

--- client/src/Navbar/Navbar.jsx
+++ client/src/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import { TiGroupOutline } from 'react-icons/ti';
 import { useState, useContext, useEffect, useRef } from 'react';
 import { UserContext } from "../UserContext"
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 const Navbar = () => {
 
     const [dropdownOpen, setDropdownOpen] = useState(false);
@@ -35,26 +35,38 @@ const Navbar = () => {
         navigate("/unityBoard", {state: {unity_id: currentUserDetails.current_unity_id, unity_name: currentUserDetails.current_unity_name, user_id: currentUserDetails.current_user_id}})
     }
 
+    const location = useLocation()
+
     return (
 
         <nav className="fixed top-0 z-50 w-full bg-gray-900 border-b border-gray-200 dark:bg-black dark:border-gray-700">
         <div className="px-3 py-3 lg:px-5 lg:pl-3">
             <div className="flex items-center justify-between">
-            <div className="flex items-center justify-start rtl:justify-end">
-                <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-                    <span className="sr-only">Open sidebar</span>
-                    <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-                    </svg>
-                </button>
-                <a className="flex ms-2 md:me-24">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 me-3" alt="FlowBite Logo" />
-                    <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">CommUnityHub</span>
-                </a>
-            </div>
-            <div className="flex items-center">
-                <div className="flex items-center ms-3">
-                    <div className="flex justify-between items-center">
+                <div className="flex items-center justify-start rtl:justify-end">
+                    <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                        <span className="sr-only">Open sidebar</span>
+                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+                        </svg>
+                    </button>
+                    <a className="flex ms-2 md:me-24">
+                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 me-3" alt="FlowBite Logo" />
+                        <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">CommUnityHub</span>
+                    </a>
+                </div>
+                {location.pathname.includes("unityBoard") && currentUserDetails.current_unity_name && currentUserDetails.current_unity_name != "" && (
+                    <div className="flex items-center justify-start rtl:justify-end">
+                        <a className="flex ms-2 md:me-24">
+                        <div className="w-5 h-5 mr-2 bg-gray-500 text-white rounded-full flex items-center justify-center font-semibold text-sm shadow-md">
+                            {currentUserDetails.current_unity_name.charAt(0).toUpperCase()}
+                        </div>
+                            <span className="self-center font-semibold sm:text-sm whitespace-nowrap dark:text-white">{currentUserDetails.current_unity_name}</span>
+                        </a>
+                    </div>
+                )}
+                <div className="flex items-center">
+                    <div className="flex items-center ms-3">
+                        <div className="flex justify-between items-center">
                             <div className="text-base list-none divide-y divide-gray-100 rounded" id="dropdown-user">
                             <button id="unityDropdown" onClick={toggleDropdown} className="relative flex mx-2 text-sm rounded-full dark:focus:ring-gray-600" type="button">
                                 <span className="sr-only group-hover:block absolute top-full mt-1 text-white bg-gray-800 rounded-md p-1">Open user menu</span>
